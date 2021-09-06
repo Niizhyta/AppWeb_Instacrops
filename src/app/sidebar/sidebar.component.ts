821805d5aeb70d8bb7dbf2b1f54b7583fad4570a
Niizhyta <niicole.ivette@gmail.com>
2021-09-05 23:33:33 -0300

    modificación fast

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -4,19 +4,16 @@ import { SeleccionServiceService } from '../services/seleccion-service.service';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
-
-  constructor(private SeleccionService: SeleccionServiceService) { }
-  public listaItems:Array<any> = []
-
+  constructor(private SeleccionService: SeleccionServiceService) {}
+  public listaItems: Array<any> = [];
 
   ngOnInit(): void {
-    this.SeleccionService.disparadorSeleccion.subscribe(data => {
-      console.log('recibiendo data...',data);
+    this.SeleccionService.disparadorSeleccion.subscribe((data) => {
+      console.log('recibiendo data...', data);
       this.listaItems.push(data);
-      })
+    });
   }
-
 }
